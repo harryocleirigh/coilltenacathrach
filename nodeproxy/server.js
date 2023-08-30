a2cef87adb395faab6a5248d4b6c8e08b5498401
harryocleirigh <91558668+harryocleirigh@users.noreply.github.com>
2023-08-30 21:27:19 +0100

    revised ip for prod

--- nodeproxy/server.js
+++ nodeproxy/server.js
@@ -77,7 +77,7 @@ app.get('/debug/cache', (req, res) => {
 app.use('/trees', cacheMiddleware(3600));
 app.use('/trees', createProxyMiddleware({ 
 
-    target: 'http://localhost:5000',
+    target: 'http://34.254.173.244:5000',
     changeOrigin: true,
     selfHandleResponse: true, // Add this line to handle the response ourselves
     onProxyRes: (proxyRes, req, res) => {
@@ -118,9 +118,9 @@ app.use('/trees', createProxyMiddleware({
   }  
 }));
 
-app.use('/singletree', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true }));
+app.use('/singletree', createProxyMiddleware({ target: 'http://34.254.173.244:5000', changeOrigin: true }));
 
 // launch app
 app.listen(PORT, () => {
-  console.log('Node.js proxy server is running on http://localhost:8000');
+  console.log('Node.js proxy server is running on http://34.254.173.244:8000');
 });
